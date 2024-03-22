4734bbdb6f6120b20f023618d1f80b8df73b2afc
minhpv17 <minhpv17@fpt.com>
2024-03-22 16:51:59 +0700

    adjust colorscheme

--- cyberpunk_2077/lua/lush_theme/cyberpunk_2077.lua
+++ cyberpunk_2077/lua/lush_theme/cyberpunk_2077.lua
@@ -67,14 +67,14 @@ local theme = lush(function(injected_functions)
 		--
 		-- See :h highlight-groups
 		--
-		-- ColorColumn    { }, -- Columns set with 'colorcolumn'
-		-- Conceal        { }, -- Placeholder characters substituted for concealed text (see 'conceallevel')
+		ColorColumn({}), -- Columns set with 'colorcolumn'
+		Conceal({}), -- Placeholder characters substituted for concealed text (see 'conceallevel')
 		Cursor({ bg = red }), -- Character under the cursor
 		CurSearch({ bg = blue.de(70), gui = "bold" }), -- Highlighting a search pattern under the cursor (see 'hlsearch')
 		-- lCursor        { }, -- Character under the cursor when |language-mapping| is used (see 'guicursor')
 		-- CursorIM       { }, -- Like Cursor, but used when in IME mode |CursorIM|
 		-- CursorColumn   { }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
-		-- Directory      { }, -- Directory names (and other special names in listings)
+		Directory({}), -- Directory names (and other special names in listings)
 		DiffAdd({ fg = yellow }), -- Diff mode: Added line |diff.txt|
 		DiffChange({ fg = blue }), -- Diff mode: Changed line |diff.txt|
 		DiffDelete({ fg = red }), -- Diff mode: Deleted line |diff.txt|
@@ -84,11 +84,11 @@ local theme = lush(function(injected_functions)
 		-- TermCursorNC   { }, -- Cursor in an unfocused terminal
 		-- ErrorMsg       { }, -- Error messages on the command line
 		VertSplit({ gui = "bold" }), -- Column separating vertically split windows
-		-- Folded         { }, -- Line used for closed folds
-		-- FoldColumn     { }, -- 'foldcolumn'
-		-- SignColumn     { }, -- Column where |signs| are displayed
-		-- IncSearch      { }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
-		-- Substitute     { }, -- |:substitute| replacement text highlighting
+		Folded({}), -- Line used for closed folds
+		FoldColumn({}), -- 'foldcolumn'
+		SignColumn({}), -- Column where |signs| are displayed
+		IncSearch({}), -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
+		Substitute({}), -- |:substitute| replacement text highlighting
 		LineNr({ fg = blue }), -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
 		-- LineNrAbove    { }, -- Line number for when the 'relativenumber' option is set, above the cursor line
 		-- LineNrBelow    { }, -- Line number for when the 'relativenumber' option is set, below the cursor line
@@ -98,24 +98,24 @@ local theme = lush(function(injected_functions)
 		-- MatchParen     { }, -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
 		-- ModeMsg        { }, -- 'showmode' message (e.g., "-- INSERT -- ")
 		MsgArea({ fg = blue }), -- Area for messages and cmdline
-		-- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
-		-- MoreMsg        { }, -- |more-prompt|
-		-- NonText        { }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-		Normal({ bg = hsl("#000000"), fg = blue }), -- Normal text
-		-- NormalFloat    { }, -- Normal text in floating windows.
-		-- FloatBorder    { }, -- Border of floating windows.
-		-- FloatTitle     { }, -- Title of floating windows.
-		-- NormalNC       { }, -- normal text in non-current windows
-		Pmenu({ bg = black.li(10) }), -- Popup menu: Normal item.
-		-- PmenuSel       { }, -- Popup menu: Selected item.
+		MsgSeparator({}), -- Separator for scrolled messages, `msgsep` flag of 'display'
+		MoreMsg({}), -- |more-prompt|
+		NonText({}), -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
+		Normal({ bg = black, fg = blue }), -- Normal text
+		NormalFloat({}), -- Normal text in floating windows.
+		FloatBorder({}), -- Border of floating windows.
+		FloatTitle({}), -- Title of floating windows.
+		NormalNC({}), -- normal text in non-current windows
+		Pmenu({ bg = blue.da(90).ro(17) }), -- Popup menu: Normal item.
+		PmenuSel({ bg = yellow }), -- Popup menu: Selected item.
 		-- PmenuKind      { }, -- Popup menu: Normal item "kind"
 		-- PmenuKindSel   { }, -- Popup menu: Selected item "kind"
 		-- PmenuExtra     { }, -- Popup menu: Normal item "extra text"
 		-- PmenuExtraSel  { }, -- Popup menu: Selected item "extra text"
 		-- PmenuSbar      { }, -- Popup menu: Scrollbar.
 		-- PmenuThumb     { }, -- Popup menu: Thumb of the scrollbar.
-		-- Question       { }, -- |hit-enter| prompt and yes/no questions
-		-- QuickFixLine   { }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
+		Question({}), -- |hit-enter| prompt and yes/no questions
+		QuickFixLine({}), -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
 		NoiceCmdlinePopupBorderCmdline({ fg = blue, gui = "bold" }),
 		NoiceCmdlineIconCmdline({ fg = blue, gui = "bold" }),
 		NoiceCmdlinePopupBorderHelp({ fg = blue, gui = "bold" }),
@@ -130,7 +130,7 @@ local theme = lush(function(injected_functions)
 		-- TabLine        { }, -- Tab pages line, not active tab page label
 		-- TabLineFill    { }, -- Tab pages line, where there are no labels
 		-- TabLineSel     { }, -- Tab pages line, active tab page label
-		-- Title          { }, -- Titles for output from ":set all", ":autocmd" etc.
+		Title({}), -- Titles for output from ":set all", ":autocmd" etc.
 		Visual({ bg = blue, fg = red, gui = "bold" }), -- Visual mode selection
 		-- VisualNOS      { }, -- Visual mode selection when vim is "Not Owning the Selection".
 		-- WarningMsg     { }, -- Warning messages
@@ -140,7 +140,7 @@ local theme = lush(function(injected_functions)
 		-- WinBar         { }, -- Window bar of current window
 		-- WinBarNC       { }, -- Window bar of not-current windows
 
-		CursorLine({ bg = Normal.bg.lighten(10) }), --- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
+		CursorLine({ bg = blue.da(86).ro(16) }), --- Screen-line at the cursor, when 'cursorline' is set. Low-priority if foreground (ctermfg OR guifg) is not set.
 
 		-- Common vim syntax groups used for all kinds of code and markup.
 		-- Commented-out groups should chain up to their preferred (*) group
@@ -199,44 +199,44 @@ local theme = lush(function(injected_functions)
 
 		-- See :h lsp-highlight, some groups may not be listed, submit a PR fix to lush-template!
 		--
-		-- LspReferenceText            { } , -- Used for highlighting "text" references
-		-- LspReferenceRead            { } , -- Used for highlighting "read" references
-		-- LspReferenceWrite           { } , -- Used for highlighting "write" references
-		-- LspCodeLens                 { } , -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
-		-- LspCodeLensSeparator        { } , -- Used to color the seperator between two or more code lens.
-		-- LspSignatureActiveParameter { } , -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
+		LspReferenceText({}), -- Used for highlighting "text" references
+		LspReferenceRead({}), -- Used for highlighting "read" references
+		LspReferenceWrite({}), -- Used for highlighting "write" references
+		LspCodeLens({}), -- Used to color the virtual text of the codelens. See |nvim_buf_set_extmark()|.
+		LspCodeLensSeparator({}), -- Used to color the seperator between two or more code lens.
+		LspSignatureActiveParameter({}), -- Used to highlight the active parameter in the signature help. See |vim.lsp.handlers.signature_help()|.
 
 		-- See :h diagnostic-highlights, some groups may not be listed, submit a PR fix to lush-template!
 		--
-		DiagnosticError({ fg = red, bg = red.da(80).de(20), gui = "bold" }), -- U fg = yellow, bg = yellow.da(85).de(20)sed as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticError({ fg = red, bg = red.da(83).de(20), gui = "bold" }), -- U fg = yellow, bg = yellow.da(85).de(20)sed as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		DiagnosticWarn({ fg = yellow, bg = yellow.da(85).de(20) }), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticInfo             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticHint             { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
-		-- DiagnosticOk               { } , -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticInfo({}), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticHint({}), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
+		DiagnosticOk({}), -- Used as the base highlight group. Other Diagnostic highlights link to this by default (except Underline)
 		DiagnosticVirtualTextError({
 			fg = red,
 			bg = red.da(80).de(20),
 			gui = "bold",
 		}), -- Used for "Error" diagnostic virtual text.
-		DiagnosticVirtualTextWarn({ fg = yellow, bg = yellow.da(85).de(20) }), -- Used for "Warn" diagnostic virtual text.
-		-- DiagnosticVirtualTextInfo  { } , -- Used for "Info" diagnostic virtual text.
-		-- DiagnosticVirtualTextHint  { } , -- Used for "Hint" diagnostic virtual text.
-		-- DiagnosticVirtualTextOk    { } , -- Used for "Ok" diagnostic virtual text.
-		-- DiagnosticUnderlineError   { } , -- Used to underline "Error" diagnostics.
-		-- DiagnosticUnderlineWarn    { } , -- Used to underline "Warn" diagnostics.
-		-- DiagnosticUnderlineInfo    { } , -- Used to underline "Info" diagnostics.
-		-- DiagnosticUnderlineHint    { } , -- Used to underline "Hint" diagnostics.
-		-- DiagnosticUnderlineOk      { } , -- Used to underline "Ok" diagnostics.
-		-- DiagnosticFloatingError    { } , -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
-		-- DiagnosticFloatingWarn     { } , -- Used to color "Warn" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingInfo     { } , -- Used to color "Info" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingHint     { } , -- Used to color "Hint" diagnostic messages in diagnostics float.
-		-- DiagnosticFloatingOk       { } , -- Used to color "Ok" diagnostic messages in diagnostics float.
-		-- DiagnosticSignError        { } , -- Used for "Error" signs in sign column.
-		-- DiagnosticSignWarn         { } , -- Used for "Warn" signs in sign column.
-		-- DiagnosticSignInfo         { } , -- Used for "Info" signs in sign column.
-		-- DiagnosticSignHint         { } , -- Used for "Hint" signs in sign column.
-		-- DiagnosticSignOk           { } , -- Used for "Ok" signs in sign column.
+		DiagnosticVirtualTextWarn({ fg = yellow, bg = yellow.da(83).de(20) }), -- Used for "Warn" diagnostic virtual text.
+		DiagnosticVirtualTextInfo({}), -- Used for "Info" diagnostic virtual text.
+		DiagnosticVirtualTextHint({}), -- Used for "Hint" diagnostic virtual text.
+		DiagnosticVirtualTextOk({}), -- Used for "Ok" diagnostic virtual text.
+		DiagnosticUnderlineError({}), -- Used to underline "Error" diagnostics.
+		DiagnosticUnderlineWarn({}), -- Used to underline "Warn" diagnostics.
+		DiagnosticUnderlineInfo({}), -- Used to underline "Info" diagnostics.
+		DiagnosticUnderlineHint({}), -- Used to underline "Hint" diagnostics.
+		DiagnosticUnderlineOk({}), -- Used to underline "Ok" diagnostics.
+		DiagnosticFloatingError({}), -- Used to color "Error" diagnostic messages in diagnostics float. See |vim.diagnostic.open_float()|
+		DiagnosticFloatingWarn({}), -- Used to color "Warn" diagnostic messages in diagnostics float.
+		DiagnosticFloatingInfo({}), -- Used to color "Info" diagnostic messages in diagnostics float.
+		DiagnosticFloatingHint({}), -- Used to color "Hint" diagnostic messages in diagnostics float.
+		DiagnosticFloatingOk({}), -- Used to color "Ok" diagnostic messages in diagnostics float.
+		DiagnosticSignError({}), -- Used for "Error" signs in sign column.
+		DiagnosticSignWarn({}), -- Used for "Warn" signs in sign column.
+		DiagnosticSignInfo({}), -- Used for "Info" signs in sign column.
+		DiagnosticSignHint({}), -- Used for "Hint" signs in sign column.
+		DiagnosticSignOk({}), -- Used for "Ok" signs in sign column.
 
 		-- Tree-Sitter syntax groups.
 		--
