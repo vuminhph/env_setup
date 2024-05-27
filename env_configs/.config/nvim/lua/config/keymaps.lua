53e569dac107e639507f13b394d7887c0b9aa6fd
minhpv17 <minhpv17@fpt.com>
2024-05-27 16:44:47 +0700

    add toggleterm

--- env_configs/.config/nvim/lua/config/keymaps.lua
+++ env_configs/.config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-Util = require("lazyvim.util")
-vim.keymap.set("n", "<C-\\>", function()
-  Util.terminal(nil, { border = "rounded" })
-end, { desc = "Term with border" })
+-- Util = require("lazyvim.util")
+-- vim.keymap.set("n", "<C-\\>", function()
+--   Util.terminal(nil, { border = "rounded" })
+-- end, { desc = "Term with border" })
